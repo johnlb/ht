43a1416c1aa2a87339959b2a493b323cdd5462e2
John Bell <john.l.bell@gmail.com>
2016-10-21 04:15:59 -0400

    adding build requirements

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    # 'sphinx.ext.githubpages',
     'sphinxcontrib.matlab',
 ]
 
